6bffd36dd4648d588997133ba30ac05ea7d60fb7
Drailan John Terrible <drailanjohn.gss@gmail.com>
2018-10-03 13:48:56 +0800

    implemented about and projects page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+ $(document).ready(function(){
+     $('.center').slick({
+       centerMode: true,
+       centerPadding: '60px',
+       slidesToShow: 3,
+       variableWidth: true,
+       // autoplay: true,
+       autoplaySpeed: 2000,
+     });
+ });
