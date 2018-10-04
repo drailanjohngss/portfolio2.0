f84195c6b18043f1d1ce3e31d5d44d668eab650e
Drailan John Terrible <drailanjohn.gss@gmail.com>
2018-10-04 15:29:30 +0800

    update

--- js/script.js
+++ js/script.js
@@ -5,7 +5,19 @@
        centerPadding: '60px',
        slidesToShow: 3,
        variableWidth: true,
-       autoplay: true,
+       // autoplay: true,
        autoplaySpeed: 2000,
+     });
+
+     var vSequence = [
+       { elements: $('.bsquare2-img'), properties: 'slideDown', options: { duration: 1500 } }
+     ];
+
+
+     $(".bsquare2-img").lazy({
+         afterLoad: function(element) {
+             // called after an element was successfully handled
+             $.Velocity.RunSequence(vSequence);
+         }
      });
  });
